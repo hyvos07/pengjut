6cc61ac4251a1b3a8fcba7073b95a37db115191c
Hyvos <daniel.liman07@gmail.com>
2025-02-20 21:49:25 +0700

    refactor: rename home page controller file

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("")
-public class Home {
+public class HomePageController {
     @GetMapping("/")
     public String homePage() {
         return "HomePage";
