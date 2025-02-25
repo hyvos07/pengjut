fbe38a46e9c6c56a067b7ae248a0abde66f5ef8c
Hyvos <daniel.liman07@gmail.com>
2025-02-25 15:47:36 +0700

    feat: Repository for Car

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -0,0 +1,59 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class CarRepository {
+    static int id = 0;
+    private List<Car> carData = new ArrayList<>();
+
+    public Car create(Car car){
+        if(car.getCarId() == null){
+            car.setCarId(UUID.randomUUID().toString());
+        }
+        
+        carData.add(car);
+        
+        return car;
+    }
+
+    public Iterator<Car> findAll(){
+        return carData.iterator();
+    }
+
+    public Car findById(String carId){
+        for(Car car : carData){
+            if(car.getCarId().equals(carId)){
+                return car;
+            }
+        }
+
+        return null;
+    }
+
+    public Car update(String id, Car updatedCar){
+        for (int i = 0; i < carData.size(); i++){
+            Car car = carData.get(i);
+            if (car.getCarId().equals(id)){
+                // Change if same
+                car.setCarName(updatedCar.getCarName());
+                car.setCarColor(updatedCar.getCarColor());
+                car.setCarQuantity(updatedCar.getCarQuantity());
+                
+                return updatedCar;
+            }
+        }
+
+        return null; // When there is no car with the given id
+    }
+
+    public void delete(String id){
+        carData.removeIf(car -> car.getCarId().equals(id));
+    }
+}
