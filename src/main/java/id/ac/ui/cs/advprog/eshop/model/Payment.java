ae07b67a6a24f0be2b193acbc0eec4918200dff9
Hyvos <daniel.liman07@gmail.com>
2025-03-07 11:53:21 +0700

    feat: [GREEN] add Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
 import java.util.Map;
+import java.util.UUID;
 
 import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
@@ -16,22 +17,64 @@ public class Payment {
     private Order order;
 
     public Payment(PaymentMethod method, Map<String, String> paymentData, Order order) {
-        
+        this.id = UUID.randomUUID().toString();
+
+        this.setMethod(method);
+        this.setStatus(PaymentStatus.PENDING);
+        this.setPaymentData(paymentData);
+
+        if (order == null) {
+            throw new IllegalArgumentException();
+        } else {
+            this.order = order;
+        }
     }
 
     public Payment(PaymentMethod method, PaymentStatus status, Map<String, String> paymentData, Order order) {
-        
+        this(method, paymentData, order);
+        this.setStatus(status);
     }
 
     public void setMethod(PaymentMethod method) throws IllegalArgumentException {
-        
+        if (method != null) {
+            this.method = method;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void setStatus(PaymentStatus status) throws IllegalArgumentException {
-        
+        if (status != null) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void setPaymentData(Map<String, String> paymentData) throws IllegalArgumentException {
-
+        if (paymentData == null) throw new IllegalArgumentException();
+        
+        if (this.method == PaymentMethod.VOUCHER) {
+            if (!(
+                paymentData.get("voucherCode").length() == 16 &&                        // Voucher code length is 16
+                paymentData.get("voucherCode").startsWith("ESHOP") &&                   // Voucher code starts with "ESHOP"
+                paymentData.get("voucherCode").replaceAll("[^0-9]", "").length() == 8   // Voucher code contains exactly 8 digits
+            )) {
+                throw new IllegalArgumentException();
+            } else {
+                this.paymentData = paymentData;
+            }
+        } else {
+            if (
+                paymentData.get("address") == null ||       // Address data is exists
+                paymentData.get("address").isEmpty() ||
+                paymentData.get("deliveryFee") == null ||   // Delivery fee data is exists
+                paymentData.get("deliveryFee").isEmpty()
+            ) {
+                throw new IllegalArgumentException();
+            } else {
+                this.paymentData = paymentData;
+            }
+        }
     }
 }
