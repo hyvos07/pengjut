72f9e3e0c51061d6e2d05146c3a94149c073d8ac
Hyvos <daniel.liman07@gmail.com>
2025-03-07 12:33:54 +0700

    refactor: remove contains check

--- src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentMethod.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentMethod.java
@@ -1,15 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.enums;
 
 public enum PaymentMethod {
-    VOUCHER(),
-    COD();
-
-    public static boolean contains(String param) {
-        for (OrderStatus orderStatus : OrderStatus.values()) {
-            if (orderStatus.name().equals(param)) {
-                return true;
-            }
-        }
-        return false;
-    }
+    VOUCHER,
+    COD;
 }
--- src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentStatus.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentStatus.java
@@ -1,16 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.enums;
 
 public enum PaymentStatus {
-    SUCCESS(),
-    REJECTED(),
-    PENDING();
-
-    public static boolean contains(String param) {
-        for (OrderStatus orderStatus : OrderStatus.values()) {
-            if (orderStatus.name().equals(param)) {
-                return true;
-            }
-        }
-        return false;
-    }
+    SUCCESS,
+    REJECTED,
+    PENDING;
 }
